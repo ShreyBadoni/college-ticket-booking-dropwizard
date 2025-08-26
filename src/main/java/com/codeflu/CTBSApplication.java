a939b27fe37f956be419a85902f456c83b0b815f
ShreyBadoni <shreybadoni23@gmail.com>
2025-08-27 01:15:01 +0530

    add booking and event resources with corresponding DAOs and services

--- src/main/java/com/codeflu/CTBSApplication.java
+++ src/main/java/com/codeflu/CTBSApplication.java
@@ -1,6 +1,12 @@
 package com.codeflu;
 
+import com.codeflu.Dao.BookingDAO;
+import com.codeflu.Dao.EventDAO;
+import com.codeflu.Resources.BookingResource;
+import com.codeflu.Resources.EventResource;
 import com.codeflu.Resources.HelloResource;
+import com.codeflu.Services.BookingService;
+import com.codeflu.Services.EventService;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
@@ -19,8 +25,18 @@ public class CTBSApplication extends Application<CTBSConfiguration> {
 
     @Override
     public void run(CTBSConfiguration configuration, Environment environment) {
-        // Register resources here
         final HelloResource resource = new HelloResource();
+
+        final EventDAO eventDAO = new EventDAO();
+        final EventService eventService = new EventService(eventDAO);
+        final EventResource eventResource = new EventResource(eventService);
+
+        final BookingDAO bookingDAO = new BookingDAO();
+        final BookingService bookingService = new BookingService(bookingDAO, eventService);
+        final BookingResource bookingResource = new BookingResource(bookingService,eventService);
+
         environment.jersey().register(resource);
+        environment.jersey().register(eventResource);
+        environment.jersey().register(bookingResource);
     }
 }
